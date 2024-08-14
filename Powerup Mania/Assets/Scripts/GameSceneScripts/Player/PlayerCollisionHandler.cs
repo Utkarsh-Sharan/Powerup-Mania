5e3b7d14c68b18b92e7b5989b3257a2fd4ddbdce
Utkarsh Sharan <utkarsh.sharan99@gmail.com>
2024-08-14 10:27:34 +0530

    abrupt level end scene added

--- Powerup Mania/Assets/Scripts/GameSceneScripts/Player/PlayerCollisionHandler.cs
+++ Powerup Mania/Assets/Scripts/GameSceneScripts/Player/PlayerCollisionHandler.cs
@@ -30,6 +30,12 @@ public class PlayerCollisionHandler : MonoBehaviour
 
             Destroy(other.gameObject);
         }
+        else if (other.GetComponent<LevelEndPowerup>())
+        {
+            PowerupManager.IsLevelEndPowerupActivated = true;
+
+            Destroy(other.gameObject);
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D other)
