dd8ef89dde5499899292b28bf8cfdbb2e6af45af
Utkarsh Sharan <utkarsh.sharan99@gmail.com>
2024-08-14 10:32:23 +0530

    implemented level end powerup

--- Powerup Mania/Assets/Scripts/GameSceneScripts/Player/PlayerPowerupEffectHandler.cs
+++ Powerup Mania/Assets/Scripts/GameSceneScripts/Player/PlayerPowerupEffectHandler.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerPowerupEffectHandler : MonoBehaviour
 {
@@ -40,6 +41,13 @@ public class PlayerPowerupEffectHandler : MonoBehaviour
         {
             _isCoroutineActive = true;
             StartCoroutine(InvisibilityOverRoutine());
+        }
+
+        if(!_isCoroutineActive && PowerupManager.IsLevelEndPowerupActivated)
+        {
+            _isCoroutineActive = true;
+            SceneManager.LoadScene(1);
+            PowerupManager.IsLevelEndPowerupActivated = false;
         }
     }
 
