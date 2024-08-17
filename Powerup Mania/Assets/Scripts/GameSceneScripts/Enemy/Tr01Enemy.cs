2e53974fd9bffb70a24626e5d300821227eedc44
Utkarsh Sharan <utkarsh.sharan99@gmail.com>
2024-08-17 15:21:31 +0530

    enemies now detecting player in detection zone only

--- Powerup Mania/Assets/Scripts/GameSceneScripts/Enemy/Tr01Enemy.cs
+++ Powerup Mania/Assets/Scripts/GameSceneScripts/Enemy/Tr01Enemy.cs
@@ -5,16 +5,20 @@ using UnityEngine;
 public class Tr01Enemy : MonoBehaviour
 {
     private float _moveSpeed = 2f;
+    public static bool playerInDetectionZone;
 
     private void Update()
     {
-        Transform playerTransform = PlayerTransformManager.Instance.playerTransform;
-
-        float distance = Vector2.Distance(this.transform.position, playerTransform.position);
-        if(distance < 5f)
+        if (playerInDetectionZone)
         {
-            Vector2 direction = (playerTransform.position - transform.position).normalized;
-            transform.position = Vector2.MoveTowards(transform.position, playerTransform.position, _moveSpeed * Time.deltaTime);
+            Transform playerTransform = PlayerTransformManager.Instance.playerTransform;
+
+            float distance = Vector2.Distance(this.transform.position, playerTransform.position);
+            if (distance < 5f)
+            {
+                Vector2 direction = (playerTransform.position - transform.position).normalized;
+                transform.position = Vector2.MoveTowards(transform.position, playerTransform.position, _moveSpeed * Time.deltaTime);
+            }
         }
     }
 }
